c049cb2f1ec1a97dd0bafd572822cb517ab11941
Soichi Hayashi <hayashis@iu.edu>
2021-06-04 20:23:18 +0000

    more forcefully adding parc-stats directory

--- validate.py
+++ validate.py
@@ -8,15 +8,15 @@ with open('config.json', encoding='utf8') as config_json:
 
 results = {"errors": [], "warnings": []}
 
-
 #symlink the input in - no change to the data
 if not os.path.exists("output"):
     os.mkdir("output")
 
-    if not os.path.exists(config["parc-stats"]):
-        results["errors"].append("Can't find parc-stats directory");
-    else:
-        os.symlink(config["parc-stats"], "output/parc-stats")
+if not os.path.exists(config["parc-stats"]):
+    results["errors"].append("Can't find parc-stats directory");
+else:
+    if not os.path.exists("output/parc-stats"):
+        os.symlink("../"+config["parc-stats"], "output/parc-stats")
 
 #we don't have anything for secondary but app-secondary-archive requires it
 if not os.path.exists("secondary"):
